771effe69c7b36f86f79fc2d8c3ef344e09b21c8
Francesco Galisi <f.galisi@datawizard.it>
2022-07-25 18:00:33 +0200

    Started work on servlet configuration

---
+++ src/main/java/it/datawizard/unicom/unicombackend/RestController.java
@@ -0,0 +1,32 @@
+package it.datawizard.unicom.unicombackend;
+
+
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import org.springframework.context.ApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(value = "/*", displayName = "fhir")
+public class RestController extends RestfulServer {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The initialize method is automatically called when the servlet is starting up, so it can
+     * be used to configure the servlet to define resource providers, or set up
+     * configuration, interceptors, etc.
+     */
+    @Override
+    protected void initialize() throws ServletException {
+        /*
+         * The servlet defines any number of resource providers, and
+         * configures itself to use them by calling
+         * setResourceProviders()
+         */
+        List<IResourceProvider> resourceProviders = new ArrayList<IResourceProvider>();
+        setResourceProviders(resourceProviders);
+    }
+}
--- src/main/java/it/datawizard/unicom/unicombackend/UnicomBackendApplication.java
+++ src/main/java/it/datawizard/unicom/unicombackend/UnicomBackendApplication.java
@@ -1,10 +1,21 @@
 package it.datawizard.unicom.unicombackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UnicomBackendApplication {
+	@Bean
+	public ServletRegistrationBean unicomServletBean() {
+		ServletRegistrationBean bean = new ServletRegistrationBean(new RestController(), "/*");
+		bean.setLoadOnStartup(1);
+		return bean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnicomBackendApplication.class, args);
