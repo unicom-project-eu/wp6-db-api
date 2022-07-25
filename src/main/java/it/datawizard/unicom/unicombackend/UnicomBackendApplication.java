76b38dcd9983df611340a1890fa6fa8018b3d424
Francesco Galisi <f.galisi@datawizard.it>
2022-07-25 14:46:58 +0200

    Generated project

---
+++ src/main/java/it/datawizard/unicom/unicombackend/UnicomBackendApplication.java
@@ -0,0 +1,13 @@
+package it.datawizard.unicom.unicombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/datawizard/unicom/unicombackend/UnicomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.datawizard.unicom.unicombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
