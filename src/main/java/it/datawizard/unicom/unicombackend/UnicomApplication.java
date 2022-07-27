f153c8ccc7be411df400cbc12b7c5626ef8c1e61
Francesco Galisi <f.galisi@datawizard.it>
2022-07-27 11:48:12 +0200

    Moved ServletRegistrationBean inside UnicomApplication

--- src/main/java/it/datawizard/unicom/unicombackend/UnicomApplication.java
+++ src/main/java/it/datawizard/unicom/unicombackend/UnicomApplication.java
@@ -1,5 +1,6 @@
 package it.datawizard.unicom.unicombackend;
 
+import it.datawizard.unicom.unicombackend.fhir.UnicomFHIRServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -7,6 +8,13 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UnicomApplication {
+
+	@Bean
+	public ServletRegistrationBean unicomServletBean() {
+		ServletRegistrationBean bean = new ServletRegistrationBean(new UnicomFHIRServlet(), "/*");
+		bean.setLoadOnStartup(1);
+		return bean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnicomApplication.class, args);
