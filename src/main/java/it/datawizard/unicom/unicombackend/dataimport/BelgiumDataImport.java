82b09caa7202776155a8979cef9488060a51fa77
Francesco Galisi <f.galisi@datawizard.it>
2022-08-04 18:06:38 +0200

    Installed opencsv

--- src/main/java/it/datawizard/unicom/unicombackend/dataimport/BelgiumDataImport.java
+++ src/main/java/it/datawizard/unicom/unicombackend/dataimport/BelgiumDataImport.java
@@ -1,8 +1,19 @@
 package it.datawizard.unicom.unicombackend.dataimport;
 
+import com.opencsv.bean.CsvBindByName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BelgiumDataImport implements Runnable {
+    private static Logger LOG = LoggerFactory.getLogger(BelgiumDataImport.class);
+
     @Override
     public void run() {
 
     }
+}
+
+class BelgiumDataBean extends CsvBean {
+    @CsvBindByName
+
 }
--- src/main/java/it/datawizard/unicom/unicombackend/dataimport/FinlandDataImport.java
+++ src/main/java/it/datawizard/unicom/unicombackend/dataimport/FinlandDataImport.java
@@ -1,8 +1,12 @@
 package it.datawizard.unicom.unicombackend.dataimport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FinlandDataImport implements Runnable {
+    private static Logger LOG = LoggerFactory.getLogger(FinlandDataImport.class);
+
     @Override
     public void run() {
-
     }
 }
