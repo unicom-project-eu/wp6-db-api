7b53a2f75916211b926e5c0ab200ca3acb3e8300
Francesco Galisi <f.galisi@datawizard.it>
2022-08-05 17:14:48 +0200

    Renamed PackagedProduct into PackagedMedicinalProduct

--- src/main/java/it/datawizard/unicom/unicombackend/datamodel/PackagedMedicinalProduct.java
+++ src/main/java/it/datawizard/unicom/unicombackend/datamodel/PackagedMedicinalProduct.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 @Entity
 @Setter
 @Getter
-public class PackagedProduct {
+public class PackagedMedicinalProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
