364e2488a5ad117787b767e6e916f2e11f7f30b8
Francesco Galisi <f.galisi@datawizard.it>
2022-08-11 12:24:36 +0200

    Created script to preprocess substance data

---
+++ preprocess-data/substance/preprocess.py
@@ -0,0 +1,42 @@
+import csv
+import re
+
+ORIGINA_PATH = "original.csv"
+OUTPUT_PATH = "substances_pai.csv"
+
+if __name__ == '__main__':
+    print(f'Parsing "{ORIGINA_PATH}"...')
+
+    output_rows = []
+
+    with open(ORIGINA_PATH, 'r') as csvin:
+        csvreader = csv.DictReader(csvin, delimiter=',')
+
+        for row in csvreader:
+            substance_pai_tokens = re.split(r'\s+', row['substance_pai'].lower())
+
+            moiety = substance_pai_tokens[0]
+            
+            modifier = ' '.join(substance_pai_tokens[1:])
+            if modifier == '':
+                modifier = None
+
+            ingredient_code = row['ingredient_code'].strip()
+            if not re.match(r'^\d+$', ingredient_code):
+                ingredient_code = None
+
+            output_rows.append({
+                'moiety': moiety,
+                'modifier': modifier,
+                'ingredient_code': ingredient_code,
+            })
+        pass
+
+    print(f'Writing output to "{OUTPUT_PATH}"...')
+
+    with open(OUTPUT_PATH, 'w') as csvout:
+        csvwriter = csv.DictWriter(csvout, list((output_rows[0].keys())))
+        csvwriter.writeheader()
+        csvwriter.writerows(output_rows)
+    
+    print('Done')
