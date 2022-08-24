e0c372a2aba209d520802cd41260d47fefe85727
Francesco Galisi <f.galisi@datawizard.it>
2022-08-24 10:10:12 +0200

    Removed useless import

--- src/main/java/it/datawizard/unicom/unicombackend/UnicomApplication.java
+++ src/main/java/it/datawizard/unicom/unicombackend/UnicomApplication.java
@@ -1,8 +1,6 @@
 package it.datawizard.unicom.unicombackend;
 
 import it.datawizard.unicom.unicombackend.fhir.UnicomFHIRServlet;
-import it.datawizard.unicom.unicombackend.jpa.repository.SubstanceWithRolePaiRepository;
-import kotlin.annotation.Retention;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
 
 @SpringBootApplication(scanBasePackages = "it.datawizard.unicom.unicombackend")
 public class UnicomApplication implements CommandLineRunner {
