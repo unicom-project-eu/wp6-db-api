3dd7ead26cb0e78d25b8f4b6801f1ff07a1d61a2
Francesco Galisi <f.galisi@datawizard.it>
2022-09-27 18:06:29 +0200

    Fixed integration test

---
+++ src/test/java/it/datawizard/unicom/unicombackend/elasticsearch/repository/MedicinalProductRepositoryTest.java
@@ -0,0 +1,11 @@
+package it.datawizard.unicom.unicombackend.elasticsearch.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinalProductRepositoryTest {
+    @Test
+    void contextLoads() {
+    }
+}
