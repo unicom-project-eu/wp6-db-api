6b485f374e8c42163ec091022433a5e107af5e4e
Francesco Galisi <f.galisi@datawizard.it>
2022-09-30 12:16:01 +0200

    Added columns to EdqmConcept

--- src/main/java/it/datawizard/unicom/unicombackend/jpa/entity/edqm/EdqmConcept.java
+++ src/main/java/it/datawizard/unicom/unicombackend/jpa/entity/edqm/EdqmConcept.java
@@ -1,17 +1,23 @@
 package it.datawizard.unicom.unicombackend.jpa.entity.edqm;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class EdqmConcept {
     @Id
     private String code;
 
     @Column(nullable = false)
     private String term;
+
+    private String definition;
+    private String comment;
 }
