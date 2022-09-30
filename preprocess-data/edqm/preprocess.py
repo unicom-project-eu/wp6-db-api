536d35cfa9104b86398aef16088faedcc5faa6c2
Francesco Galisi <f.galisi@datawizard.it>
2022-09-30 12:37:38 +0200

    Added script to preprocess all edqm concepts

---
+++ preprocess-data/edqm/preprocess.py
@@ -0,0 +1,25 @@
+from os import listdir
+from os.path import isfile, join
+
+import os
+import pandas as pd
+
+ORIGINA_DIR = './original'
+OUT_DIR = './processed'
+
+def json_to_csv(f):
+    df = pd.read_json(join(ORIGINA_DIR, f))
+    df = df.rename(columns={'isocode': 'code'})
+
+    df['code'] = df['code'].apply(lambda x: x.split('-')[1])
+
+    df.to_csv(join(OUT_DIR, f.replace('.json', '.csv')), index = None)
+
+
+if __name__ == '__main__':
+    for f in listdir(ORIGINA_DIR):
+        if not isfile(join(ORIGINA_DIR, f)):
+            continue
+
+        json_to_csv(f)
+    pass
