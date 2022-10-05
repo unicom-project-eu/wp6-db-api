aa17fe5d6985ddac606d25ad16d918bf3e20cc47
Francesco Galisi <f.galisi@datawizard.it>
2022-10-05 15:51:58 +0200

    Mapped Strength

--- preprocess-data/belgium_subset/preprocess.py
+++ preprocess-data/belgium_subset/preprocess.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import numpy as np
 import pandas as pd
 
 from csvutils.csvutils import *
@@ -9,12 +10,56 @@ def mocked_value(val):
 ORIGINA_PATH = 'original.csv'
 
 @csv_mapping(
+    concentrationNumeratorValue=AttributeInfo(set_value=lambda x: x['concentrationNumeratorValue']),
+    concentrationDenominatorValue=AttributeInfo(set_value=lambda x: x['concentrationDenominatorValue']),
+    concentrationNumeratorUnit=AttributeInfo(set_value=lambda x: x['concentrationNumeratorUnit']),
+    concentrationDenominatorUnit=AttributeInfo(set_value=lambda x: x['concentrationDenominatorUnit']),
+    presentationNumeratorValue=AttributeInfo(set_value=lambda x: x['presentationNumeratorValue']),
+    presentationDenominatorValue=AttributeInfo(set_value=lambda x: x['presentationDenominatorValue']),
+    presentationNumeratorUnit=AttributeInfo(set_value=lambda x: x['presentationNumeratorUnit']),
+    presentationDenominatorUnit=AttributeInfo(set_value=lambda x: x['presentationDenominatorUnit']),
+)
+class Strength:
+    pass
+
+@csv_mapping(
+    substancePrimaryKey=AttributeInfo(is_key=True, set_value=lambda x: x['substancePrimaryKey']),
+    substanceCode=AttributeInfo(set_value=lambda x: x['substanceCode']),
+    substanceName=AttributeInfo(set_value=lambda x: x['substanceName']),
+    moietyCode=AttributeInfo(set_value=lambda x: x['moietyCode']),
+    moietyName=AttributeInfo(set_value=lambda x: x['moietyName']),
+    referenceStrength=AttributeInfo(set_value=lambda x: Strength({
+        "concentrationNumeratorValue": x["referenceStrengthConcentrationNumeratorValue"],
+        "concentrationDenominatorValue": x["referenceStrengthConcentrationDenominatorValue"],
+        "concentrationNumeratorUnit": x["referenceStrengthConcentrationNumeratorUnit"],
+        "concentrationDenominatorUnit": x["referenceStrengthConcentrationDenominatorUnit"],
+        "presentationNumeratorValue": x["referenceStrengthPresentationNumeratorValue"],
+        "presentationDenominatorValue": x["referenceStrengthPresentationDenominatorValue"],
+        "presentationNumeratorUnit": x["referenceStrengthPresentationNumeratorUnit"],
+        "presentationDenominatorUnit": x["referenceStrengthPresentationDenominatorUnit"],
+    })),
+    strength=AttributeInfo(set_value=lambda x: Strength({
+        "concentrationNumeratorValue": x["strengthConcentrationNumeratorValue"],
+        "concentrationDenominatorValue": x["strengthConcentrationDenominatorValue"],
+        "concentrationNumeratorUnit": x["strengthConcentrationNumeratorUnit"],
+        "concentrationDenominatorUnit": x["strengthConcentrationDenominatorUnit"],
+        "presentationNumeratorValue": x["strengthPresentationNumeratorValue"],
+        "presentationDenominatorValue": x["strengthPresentationDenominatorValue"],
+        "presentationNumeratorUnit": x["strengthPresentationNumeratorUnit"],
+        "presentationDenominatorUnit": x["strengthPresentationDenominatorUnit"],
+    })),
+)
+class Substance:
+    pass
+
+
+@csv_mapping(
     pharmaceuticalProductPrimaryKey=AttributeInfo(is_key=True, set_value=lambda x: x['pharmaceuticalProductPrimaryKey']),
     administrableDoseForm=AttributeInfo(set_value=lambda x: x['administrableDoseForm']),
     pharmaceuticalProductUnitOfPresentation=AttributeInfo(set_value=lambda x: x['pharmaceuticalProductUnitOfPresentation']),
     routesOfAdministration=AttributeInfo(set_value=lambda x: x['routesOfAdministration']),
     ingredientRole=AttributeInfo(set_value=lambda x: x['ingredientRole']),
-    
+    substance=AttributeInfo(set_value=lambda x: Substance(x)),
 )
 class PharmaceuticalProduct:
     pass
@@ -65,6 +110,7 @@ if __name__ == '__main__':
         'strengthPresentationNumeratorValue': float,
         'strengthPresentationDenominatorValue': float,
     })
+    df = df.replace(np.nan, None)
 
     packages_list = []
 
