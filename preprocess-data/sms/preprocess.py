7016541b510eb168ea81f6847de2cde0fedb4591
Francesco Galisi <f.galisi@datawizard.it>
2022-10-11 17:26:47 +0200

    Preprocessed sms substances

--- preprocess-data/sms/preprocess.py
+++ preprocess-data/sms/preprocess.py
@@ -2,12 +2,23 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('sms-substances-list.csv').astype({
+    df = pd.read_csv('original.csv').astype({
         "#SMS_ID": str,
         "Is_Preferred_Name": bool,
     })
     
     df = df[df['Is_Preferred_Name'] == True]
-    print(df)
+    
+    df = pd.DataFrame({
+        'substanceCode': df['#SMS_ID'],
+        'substanceName': df['Substance_Name']
+    })
 
+    # leave empty for now
+    df['moietyCode'] = None
+    df['moietyName'] = None
+
+    df['substanceName'] = df['substanceName'].apply(lambda x: x.lower())
+
+    df.to_csv('substance.csv', index=False)
     pass
