d9e3f7e7179af3c456bab9b9b724058714f866d5
Francesco Galisi <f.galisi@datawizard.it>
2022-10-12 17:30:31 +0200

    Fetch routes of administration

--- src/main/java/it/datawizard/unicom/unicombackend/dataimport/JsonDataImporter.java
+++ src/main/java/it/datawizard/unicom/unicombackend/dataimport/JsonDataImporter.java
@@ -2,15 +2,13 @@ package it.datawizard.unicom.unicombackend.dataimport;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.datawizard.unicom.unicombackend.jpa.entity.Ingredient;
-import it.datawizard.unicom.unicombackend.jpa.entity.PackagedMedicinalProduct;
-import it.datawizard.unicom.unicombackend.jpa.entity.PharmaceuticalProduct;
-import it.datawizard.unicom.unicombackend.jpa.entity.Strength;
+import it.datawizard.unicom.unicombackend.jpa.entity.*;
 import it.datawizard.unicom.unicombackend.jpa.repository.IngredientRepository;
 import it.datawizard.unicom.unicombackend.jpa.repository.PharmaceuticalProductRepository;
 import it.datawizard.unicom.unicombackend.jpa.repository.StrengthRepository;
 import it.datawizard.unicom.unicombackend.jpa.repository.SubstanceRepository;
 import it.datawizard.unicom.unicombackend.jpa.repository.edqm.EdqmDoseFormRepository;
+import it.datawizard.unicom.unicombackend.jpa.repository.edqm.EdqmRouteOfAdministrationRepository;
 import it.datawizard.unicom.unicombackend.jpa.repository.edqm.EdqmUnitOfPresentationRepository;
 import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
@@ -22,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Component
 public class JsonDataImporter {
@@ -30,15 +29,17 @@ public class JsonDataImporter {
     private final PharmaceuticalProductRepository pharmaceuticalProductRepository;
     private final EdqmDoseFormRepository edqmDoseFormRepository;
     private final EdqmUnitOfPresentationRepository edqmUnitOfPresentationRepository;
+    private final EdqmRouteOfAdministrationRepository edqmRouteOfAdministrationRepository;
     private final IngredientRepository ingredientRepository;
     private final StrengthRepository strengthRepository;
     private final SubstanceRepository substanceRepository;
 
     @Autowired
-    public JsonDataImporter(PharmaceuticalProductRepository pharmaceuticalProductRepository, EdqmDoseFormRepository edqmDoseFormRepository, EdqmUnitOfPresentationRepository edqmUnitOfPresentationRepository, IngredientRepository ingredientRepository, StrengthRepository strengthRepository, SubstanceRepository substanceRepository) {
+    public JsonDataImporter(PharmaceuticalProductRepository pharmaceuticalProductRepository, EdqmDoseFormRepository edqmDoseFormRepository, EdqmUnitOfPresentationRepository edqmUnitOfPresentationRepository, EdqmRouteOfAdministrationRepository edqmRouteOfAdministrationRepository, IngredientRepository ingredientRepository, StrengthRepository strengthRepository, SubstanceRepository substanceRepository) {
         this.pharmaceuticalProductRepository = pharmaceuticalProductRepository;
         this.edqmDoseFormRepository = edqmDoseFormRepository;
         this.edqmUnitOfPresentationRepository = edqmUnitOfPresentationRepository;
+        this.edqmRouteOfAdministrationRepository = edqmRouteOfAdministrationRepository;
         this.ingredientRepository = ingredientRepository;
         this.strengthRepository = strengthRepository;
         this.substanceRepository = substanceRepository;
@@ -55,7 +56,7 @@ public class JsonDataImporter {
     @Transactional
     protected void saveParsedPackagedMedicinalProducts(ArrayList<PackagedMedicinalProduct> packagedMedicinalProducts) {
         for (PackagedMedicinalProduct packagedMedicinalProduct : packagedMedicinalProducts) {
-            // PharmaceuticalProduct
+            // MedicinalProduct > PharmaceuticalProduct
             PharmaceuticalProduct pharmaceuticalProduct = packagedMedicinalProduct.getMedicinalProduct()
                     .getPharmaceuticalProduct();
 
@@ -89,7 +90,20 @@ public class JsonDataImporter {
                             .orElseThrow()
             );
 
+            // PharmaceuticalProduct > routesOfAdministration
+            pharmaceuticalProduct.setRoutesOfAdministration(
+                    pharmaceuticalProduct.getRoutesOfAdministration().stream().map(
+                            edqmRouteOfAdministration -> edqmRouteOfAdministrationRepository
+                                    .findById(edqmRouteOfAdministration.getCode()).orElseThrow()
+                    ).collect(Collectors.toSet())
+            );
+
             pharmaceuticalProductRepository.save(pharmaceuticalProduct);
+
+            // MedicinalProduct
+            MedicinalProduct medicinalProduct = packagedMedicinalProduct.getMedicinalProduct();
+
+
         }
 
         throw new NotImplementedException();
