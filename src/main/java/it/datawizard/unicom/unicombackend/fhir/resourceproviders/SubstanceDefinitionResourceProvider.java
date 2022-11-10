d8c96362b5d6d198b8e35a0909cd046617441eaa
Paolo Pierantozzi <p.pierantozzi@datawizard.it>
2022-11-10 09:58:31 +0100

    Implemented multi parameter search for SubstanceDefinitionResourceProvider closes #8

--- src/main/java/it/datawizard/unicom/unicombackend/fhir/resourceproviders/SubstanceDefinitionResourceProvider.java
+++ src/main/java/it/datawizard/unicom/unicombackend/fhir/resourceproviders/SubstanceDefinitionResourceProvider.java
@@ -1,12 +1,17 @@
 package it.datawizard.unicom.unicombackend.fhir.resourceproviders;
 
 import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.OptionalParam;
 import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
+import ca.uhn.fhir.rest.param.StringParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
+import it.datawizard.unicom.unicombackend.jpa.entity.Ingredient;
 import it.datawizard.unicom.unicombackend.jpa.entity.Substance;
 import it.datawizard.unicom.unicombackend.jpa.repository.SubstanceRepository;
+import it.datawizard.unicom.unicombackend.jpa.specification.IngredientSpecifications;
+import it.datawizard.unicom.unicombackend.jpa.specification.SubstanceSpecifications;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r5.model.*;
 import org.slf4j.Logger;
@@ -14,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +28,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 public class SubstanceDefinitionResourceProvider implements IResourceProvider {
@@ -52,14 +59,26 @@ public class SubstanceDefinitionResourceProvider implements IResourceProvider {
 
     @Search
     @Transactional
-    public IBundleProvider findResources() {
+    public IBundleProvider findResources(
+            @OptionalParam(name = SubstanceDefinition.SP_CLASSIFICATION) StringParam classification,
+            @OptionalParam(name = SubstanceDefinition.SP_CODE) StringParam code,
+            @OptionalParam(name = SubstanceDefinition.SP_DOMAIN) StringParam domain,
+            @OptionalParam(name = SubstanceDefinition.SP_IDENTIFIER) StringParam identifier,
+            @OptionalParam(name = SubstanceDefinition.SP_NAME) StringParam name) {
+
         final InstantType searchTime = InstantType.withCurrentTime();
+
+        Specification<Substance> specification = Specification
+                .where(code != null ? SubstanceSpecifications.isSubstanceCodeEqualTo(code.getValue()) : null)
+                .and(name != null ? SubstanceSpecifications.isSubstanceNameEqualTo(name.getValue()) : null);
 
         return new IBundleProvider() {
 
+            final boolean shouldReturnEmptyResult = Stream.of(classification, domain, identifier).filter(bp -> bp != null).count() > 0;
+
             @Override
             public Integer size() {
-                return (int)substanceRepository.findAll(PageRequest.of(1,1)).getTotalElements();
+                return shouldReturnEmptyResult? 0 : (int)substanceRepository.findAll(specification, PageRequest.of(1,1)).getTotalElements();
             }
 
             @Nonnull
@@ -70,13 +89,14 @@ public class SubstanceDefinitionResourceProvider implements IResourceProvider {
 
                 List<IBaseResource> results = new ArrayList<>();
 
-                transactionTemplate.execute(status -> {
-                    Page<Substance> allSubstances = substanceRepository.findAll(PageRequest.of(currentPageIndex,pageSize));
-                    results.addAll(allSubstances.stream()
-                            .map(SubstanceDefinitionResourceProvider::substanceDefinitionFromEntity).toList());
-                    return null;
-                });
-
+                if(!shouldReturnEmptyResult) {
+                    transactionTemplate.execute(status -> {
+                        Page<Substance> allSubstances = substanceRepository.findAll(specification, PageRequest.of(currentPageIndex,pageSize));
+                        results.addAll(allSubstances.stream()
+                                .map(SubstanceDefinitionResourceProvider::substanceDefinitionFromEntity).toList());
+                        return null;
+                    });
+                }
                 return results;
             }
 
--- src/main/java/it/datawizard/unicom/unicombackend/jpa/repository/SubstanceRepository.java
+++ src/main/java/it/datawizard/unicom/unicombackend/jpa/repository/SubstanceRepository.java
@@ -2,9 +2,9 @@ package it.datawizard.unicom.unicombackend.jpa.repository;
 
 import it.datawizard.unicom.unicombackend.jpa.entity.Substance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface SubstanceRepository extends JpaRepository<Substance, String> {
+public interface SubstanceRepository extends JpaRepository<Substance, String>, JpaSpecificationExecutor<Substance> {
 }
